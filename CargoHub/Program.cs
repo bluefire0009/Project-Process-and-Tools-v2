493ec7fd5501a7356d49e91543050d37e99463c3
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-11-01 00:48:24 +0100

    Add  and map controllers and add db service for warehouses

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -9,10 +9,14 @@ namespace CargoHub
         static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<IWarehouseStorage, WarehouseDBStorage>();
 
             builder.Services.AddDbContext<DatabaseContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
+            app.MapControllers();
 
             app.MapGet("/", () => "Hello World!");
 
