ce8daf70cb9a265d798fdba84e0b189f4c81687a
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-11-01 00:46:11 +0100

    Write interface for Warehouse Storage

---
+++ CargoHub/Interfaces/IWarehouseStorage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using CargoHub.Models;
+
+public interface IWarehouseStorage
+{
+    IEnumerable<Warehouse> getWarehouses();
+    Task<Warehouse?> getWarehouse(int id);
+    Task<bool> addWarehouse(Warehouse warehouse);
+    Task<bool> deleteWarehouse(int id);
+    Task<bool> updateWarehouse(int id, Warehouse warehouse);
+}
