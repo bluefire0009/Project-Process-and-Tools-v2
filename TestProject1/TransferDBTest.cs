de9420471e455303c9ba400242f44596f0326630
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-11-09 21:04:14 +0100

    Add 4 actually working unit tests with description, Also add a deep copy method because a list that was supposed to not change, changed

--- TestProject1/TransferDBTest.cs
+++ TestProject1/TransferDBTest.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using CargoHub.Models;
+using System.Reflection;
+using System.Collections;
 
 namespace TestProject1;
 
@@ -115,7 +117,7 @@ public class TransferDBTest
     {
         // Arrange
         addTestResourceToDB(locations);
-        addTestResourceToDB(locations);
+        addTestResourceToDB(items);
         TransferDBStorage storage = new(db);
 
         // Act
@@ -251,34 +253,111 @@ public class TransferDBTest
 
     public static IEnumerable<object[]> CommitTransferTestData => new List<object[]>
     {
+        // Expected false because transfer amount of both transferItems is 110 and amount in inventory is 100
         new object[] { 
             new List<Inventory> {
-                new(){Id = 1, ItemId = 1, InventoryLocations = {new(){InventoryId = 1, LocationId = 1}}, total_available = 100},
-                new(){Id = 2, ItemId = 2, InventoryLocations = {new(){InventoryId = 2, LocationId = 1}}, total_available = 100}},
+                new(){Id = 1, ItemId = 1, InventoryLocations = {new(){InventoryId = 1, LocationId = 1}}, total_on_hand = 100, total_available = 100},
+                new(){Id = 2, ItemId = 2, InventoryLocations = {new(){InventoryId = 2, LocationId = 1}}, total_on_hand = 100, total_available = 100}},
             new List<Location> {new(){Id = 1}, new(){Id = 2}}, 
             new List<Item> {new(){Uid = 1}, new(){Uid = 2}}, 
-            new List<Transfer> {new(){Id = 1, TransferFrom = 1, TransferTo = 2, Items = {new(){ItemUid = 1, TransferId = 1}, new(){ItemUid = 2, TransferId = 1} }}}, 
+            new List<Transfer> {new(){
+                Id = 1, TransferFrom = 1, TransferTo = 2, 
+                Items = {
+                    new(){ItemUid = 1, TransferId = 1, Amount = 110}, 
+                    new(){ItemUid = 2, TransferId = 1, Amount = 110} }}}, 
             1,
-            true},
+            false,
+            TransferDBStorage.TransferResult.notEnoughItems},
+        // Same as above but only one amount is bigger than allowed
+        new object[] { 
+            new List<Inventory> {
+                new(){Id = 1, ItemId = 1, InventoryLocations = {new(){InventoryId = 1, LocationId = 1}}, total_on_hand = 100, total_available = 100},
+                new(){Id = 2, ItemId = 2, InventoryLocations = {new(){InventoryId = 2, LocationId = 1}}, total_on_hand = 100, total_available = 100}},
+            new List<Location> {new(){Id = 1}, new(){Id = 2}}, 
+            new List<Item> {new(){Uid = 1}, new(){Uid = 2}}, 
+            new List<Transfer> {new(){
+                Id = 1, TransferFrom = 1, TransferTo = 2, 
+                Items = {
+                    new(){ItemUid = 1, TransferId = 1, Amount = 90}, 
+                    new(){ItemUid = 2, TransferId = 1, Amount = 110} }}}, 
+            1,
+            false,
+            TransferDBStorage.TransferResult.notEnoughItems},
+        // Happy Flow test
+        new object[] { 
+            new List<Inventory> {
+                new(){Id = 1, ItemId = 1, InventoryLocations = {new(){InventoryId = 1, LocationId = 1}}, total_on_hand = 100, total_available = 100},
+                new(){Id = 2, ItemId = 2, InventoryLocations = {new(){InventoryId = 2, LocationId = 1}}, total_on_hand = 100, total_available = 100}},
+            new List<Location> {new(){Id = 1}, new(){Id = 2}}, 
+            new List<Item> {new(){Uid = 1}, new(){Uid = 2}}, 
+            new List<Transfer> {new(){
+                Id = 1, TransferFrom = 1, TransferTo = 2, 
+                Items = {
+                    new(){ItemUid = 1, TransferId = 1, Amount = 90}, 
+                    new(){ItemUid = 2, TransferId = 1, Amount = 90} }}}, 
+            1,
+            true,
+            TransferDBStorage.TransferResult.possible},
+        // Happy Flow test but inventoryLocation should also be emptied from TransferFrom
+        new object[] { 
+            new List<Inventory> {
+                new(){Id = 1, ItemId = 1, InventoryLocations = {new(){InventoryId = 1, LocationId = 1}}, total_on_hand = 100, total_available = 100},
+                new(){Id = 2, ItemId = 2, InventoryLocations = {new(){InventoryId = 2, LocationId = 1}}, total_on_hand = 100, total_available = 100}},
+            new List<Location> {new(){Id = 1}, new(){Id = 2}}, 
+            new List<Item> {new(){Uid = 1}, new(){Uid = 2}}, 
+            new List<Transfer> {new(){
+                Id = 1, TransferFrom = 1, TransferTo = 2, 
+                Items = {
+                    new(){ItemUid = 1, TransferId = 1, Amount = 100}, 
+                    new(){ItemUid = 2, TransferId = 1, Amount = 100} }}}, 
+            1,
+            true,
+            TransferDBStorage.TransferResult.possible},
     };
     [TestMethod]
     [DynamicData(nameof(CommitTransferTestData), DynamicDataSourceType.Property)]
-    public void TestCommit(List<Inventory> inventories, List<Location> locations, List<Item> items, List<Transfer> transfers, int idToCommit, bool expectedResult)
+    public void TestCommit(List<Inventory> inventories, List<Location> locations, List<Item> items, List<Transfer> transfers, int idToCommit, bool expectedResult, TransferDBStorage.TransferResult expectedMessage)
     {
         // Arrange
         addTestResourceToDB(inventories);
         addTestResourceToDB(items);
         addTestResourceToDB(locations);
-        addTestResourceToDB(items);
-        addTestResourceToDB(transfers);
+        foreach (Transfer transfer in transfers)
+        {
+            addTestResourceToDB(transfer.Items);
+
+            db.Transfers.Add(transfer);
+            db.SaveChanges();
+        }
 
         TransferDBStorage storage = new(db);
 
         // Act
+        List<Inventory> copiedInventories = DeepCopy(inventories);
         (bool succeded, TransferDBStorage.TransferResult message) actualResult = storage.commitTransfer(idToCommit).Result;
 
         // Assert
         Assert.IsTrue(actualResult.succeded == expectedResult);
+        Assert.IsTrue(actualResult.message == expectedMessage);
+        // check amount and location of each transferItem
+        foreach(Transfer transfer in db.Transfers)
+        {
+            foreach(TransferItem transferItem in transfer.Items)
+            {
+                // false uses the actually original inventories because the original won't be changed if commit fails
+                if (expectedResult == false)
+                {
+                    Assert.IsTrue(db.Inventories.ToList().SequenceEqual(inventories));
+                    Assert.IsTrue(db.InventoryLocations.Select(il => il.InventoryId).Contains(transfer.TransferFrom));
+                }
+                // true uses the copy because the inventories list gets modified in the commit method
+                if (expectedResult == true)
+                {
+                    Assert.IsTrue(!db.Inventories.ToList().SequenceEqual(copiedInventories));
+                    Assert.IsTrue(db.InventoryLocations.Select(il => il.InventoryId).Contains(transfer.TransferTo));
+                }
+            }
+        }
     }
 
     private void addTestResourceToDB<T>(List<T> resources) where T : class
@@ -292,5 +371,78 @@ public class TransferDBTest
                 db.SaveChanges();
             }
         }
+    }
+    
+
+    public T DeepCopy<T>(T obj, Dictionary<object, object> visitedObjects = null)
+    {
+        if (obj == null) return default;
+
+        // Initialize the visitedObjects dictionary if not already initialized
+        visitedObjects ??= new Dictionary<object, object>();
+
+        // If the object has already been copied, return the copy
+        if (visitedObjects.ContainsKey(obj))
+            return (T)visitedObjects[obj];
+
+        // Create a new instance of the object type
+        T copy = (T)Activator.CreateInstance(obj.GetType());
+
+        // Add the current object to the visitedObjects dictionary
+        visitedObjects[obj] = copy;
+
+        // Get all the fields (public, private, and instance)
+        foreach (var field in obj.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+        {
+            var value = field.GetValue(obj);
+
+            // Handle arrays specifically
+            if (value is Array arrayValue)
+            {
+                // Create a new array of the same type and copy the elements
+                Array copiedArray = Array.CreateInstance(arrayValue.GetType().GetElementType(), arrayValue.Length);
+
+                // Deep copy each element of the array
+                for (int i = 0; i < arrayValue.Length; i++)
+                {
+                    var element = arrayValue.GetValue(i);
+                    if (element != null && element.GetType().IsClass)
+                    {
+                        // Recursively copy each element in the array, while checking for circular references
+                        copiedArray.SetValue(DeepCopy(element, visitedObjects), i);
+                    }
+                    else
+                    {
+                        // For primitive types, just copy the value
+                        copiedArray.SetValue(element, i);
+                    }
+                }
+
+                field.SetValue(copy, copiedArray);
+            }
+            else if (value is ICollection collectionValue)
+            {
+                // For ICollection types (e.g., List<T>, HashSet<T>, etc.), create a new collection and add the cloned elements
+                var copiedCollection = (ICollection)Activator.CreateInstance(value.GetType());
+                foreach (var item in collectionValue)
+                {
+                    var copiedItem = DeepCopy(item, visitedObjects);  // Recursively copy each item
+                    copiedCollection.GetType().GetMethod("Add").Invoke(copiedCollection, new[] { copiedItem });
+                }
+                field.SetValue(copy, copiedCollection);
+            }
+            else if (value != null && value.GetType().IsClass)
+            {
+                // For reference types, recursively clone them, while checking for circular references
+                field.SetValue(copy, DeepCopy(value, visitedObjects));
+            }
+            else
+            {
+                // For primitive types, just copy the value
+                field.SetValue(copy, value);
+            }
+        }
+
+        return copy;
     }
 }
