718f0fa35f2e3d6584272c4a4542bbc992e65427
LMMosqueaVasquez <1060597@hr.nl>
2024-11-17 15:52:34 +0100

    ManagerFilter

---
+++ CargoHub/Services/ApiKeyValidateService.cs
@@ -0,0 +1,32 @@
+
+using Microsoft.EntityFrameworkCore;
+
+using CargoHub.Interface;
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace CargoHub.Services
+{
+    [ExcludeFromCodeCoverage] //Method gets called in the filter. Filter gets tested.
+    public class ApiKeyValidationService : IApiKeyValidationInterface
+    {
+        private readonly DatabaseContext _context;
+
+        
+        public ApiKeyValidationService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // Method to validate 
+        public async Task<bool> IsValidApiKeyAsync(string apiKey)
+        {
+            // Check value and if is admin
+            var exists = await _context.ApiKeys
+                                       .Where(k => k.Key_value == apiKey && k.Key_type == "admin")
+                                       .AnyAsync();
+
+            return exists;  // True exist and == admin
+        }
+    }
+}
