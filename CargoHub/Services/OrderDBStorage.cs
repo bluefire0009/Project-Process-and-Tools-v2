e6e8502a5c472205a9b5c32c339351050f606ff1
JohnSpruijt2 <johnspruijt@outlook.com>
2024-11-25 11:15:59 +0100

    removed debug console.logs

--- CargoHub/Services/OrderDBStorage.cs
+++ CargoHub/Services/OrderDBStorage.cs
@@ -288,15 +288,11 @@ public class OrderStorage : IOrderStorage
 
     public async Task<List<Location>> GetAllItemLocations(int id) {
         List<OrderItems> orderItems = await DB.OrderItems.Where(_ => _.OrderId == id).ToListAsync();
-        Console.WriteLine(orderItems);
         List<Item> items = await DB.Items.Where(_ => orderItems.Select(o => o.ItemUid).Contains(_.Uid)).ToListAsync();
-        Console.WriteLine(items);
         List<Inventory> inventories = await DB.Inventories.Where(_ => items.Select(i => i.Uid).Contains(_.ItemId)).ToListAsync();
-        Console.WriteLine(inventories);
         List<InventoryLocation> inventoryLocations = await DB.InventoryLocations.Where(_ => inventories.Select(i => i.Id).Contains(_.InventoryId)).ToListAsync();
-        Console.WriteLine(inventoryLocations);
         List<Location> Locations = await DB.Locations.Where(_ => inventoryLocations.Select(i => i.LocationId).Contains(_.Id)).ToListAsync();
-        Console.WriteLine(Locations);
+        
         return Locations;
     }
 }
