f4c1ffc7570f3dc1baa0aa969cb50be35e030c17
JohnSpruijt2 <johnspruijt@outlook.com>
2024-11-25 12:55:14 +0100

    Added extra checks to getAllItemLocations to avoid sql errors

--- CargoHub/Services/OrderDBStorage.cs
+++ CargoHub/Services/OrderDBStorage.cs
@@ -288,9 +288,13 @@ public class OrderStorage : IOrderStorage
 
     public async Task<List<Location>> GetAllItemLocations(int id) {
         List<OrderItems> orderItems = await DB.OrderItems.Where(_ => _.OrderId == id).ToListAsync();
+        if (orderItems.Count == 0) return new();
         List<Item> items = await DB.Items.Where(_ => orderItems.Select(o => o.ItemUid).Contains(_.Uid)).ToListAsync();
+        if (items.Count == 0) return new();
         List<Inventory> inventories = await DB.Inventories.Where(_ => items.Select(i => i.Uid).Contains(_.ItemId)).ToListAsync();
+        if (inventories.Count == 0) return new();
         List<InventoryLocation> inventoryLocations = await DB.InventoryLocations.Where(_ => inventories.Select(i => i.Id).Contains(_.InventoryId)).ToListAsync();
+        if (inventoryLocations.Count == 0) return new();
         List<Location> Locations = await DB.Locations.Where(_ => inventoryLocations.Select(i => i.LocationId).Contains(_.Id)).ToListAsync();
         
         return Locations;
