35498ba059bf105b646ec644f23569554806f573
JohnSpruijt2 <johnspruijt@outlook.com>
2024-12-03 11:22:09 +0100

    added item pagination unit tests

--- TestProject1/ItemDbTest.cs
+++ TestProject1/ItemDbTest.cs
@@ -48,6 +48,38 @@ public class ItemDBTest
         }
     }
 
+       public static IEnumerable<object[]> PaginationTestData => new List<object[]>
+        {
+            new object[] { 0, 0, 10, 0},
+            new object[] { 10, 0, 100, 10},
+            new object[] { 10, 5, 10, 5},
+            new object[] { 10, 10, 10, 0},
+            new object[] { 10, 0, 0, 0},
+            new object[] { 10, -1, 5, 5},
+            new object[] { 30, 10, 10, 10},
+            new object[] { 10, 10, -1, 0}
+        };
+    [TestMethod]
+    [DynamicData(nameof(PaginationTestData), DynamicDataSourceType.Property)]
+    public void TestGetPagination(int AmountItems, int offset, int limit, int expectedAmount)
+    {
+        // Arrange
+        for (int i = 0; i < AmountItems; i++)
+        {
+            Item item = new() {Uid = "P"+i};
+            db.Items.Add(item);
+            db.SaveChanges();
+        }
+
+        ItemsDBStorage storage = new(db);
+
+        // Act
+        List<Item> result = storage.GetItemsInPagination(offset, limit).Result.ToList();
+
+        // Assert
+        Assert.IsTrue(result.Count == expectedAmount);
+    }
+
     public static IEnumerable<object[]> SpecificItemTestData => new List<object[]>
         {
             new object[] { new List<Item> {}, "P00001", false},
