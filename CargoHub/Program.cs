7de89f82586b6421f3bb9caa9684ac87f8559bfc
TurboGreaser <creamsupreme666@gmail.com>
2024-12-08 13:19:55 +0100

    added swaggerUI
    use it by http://localhost:3000/swagger/

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CargoHub
 {
@@ -10,24 +12,39 @@ namespace CargoHub
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
-            
+
             builder.Services.AddScoped<IApiKeyValidationInterface, ApiKeyValidationService>();
-            builder.Services.AddScoped<IWarehouseStorage, WarehouseDBStorage>();
-            builder.Services.AddScoped<ITransferStorage, TransferDBStorage>();
-            builder.Services.AddScoped<ISupplierStorage, SupplierDBStorage>();
+
+            builder.Services.AddScoped<IClientStorage, ClientDBStorage>();
+            builder.Services.AddScoped<IInventoryStorage, InventoriesDBStorage>();
+            builder.Services.AddScoped<IItemGroupStorage, ItemGroupDBStorage>();
+            builder.Services.AddScoped<IItemLineStorage, ItemLinesDBStorage>();
             builder.Services.AddScoped<IItemStorage, ItemsDBStorage>();
             builder.Services.AddScoped<IItemTypeStorage, ItemTypesDBStorage>();
-            builder.Services.AddScoped<IItemLineStorage, ItemLinesDBStorage>();
             builder.Services.AddScoped<ILocationStorage, LocationStorage>();
             builder.Services.AddScoped<IOrderStorage, OrderStorage>();
             builder.Services.AddScoped<IShipmentStorage, ShipmentStorage>();
+            builder.Services.AddScoped<ISupplierStorage, SupplierDBStorage>();
+            builder.Services.AddScoped<ITransferStorage, TransferDBStorage>();
+            builder.Services.AddScoped<IWarehouseStorage, WarehouseDBStorage>();
+
+            // stuff for Swashbuckle.AspNetCore 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<DatabaseContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
             app.MapControllers();
 
-            app.MapGet("/", () => "Hello World!");
+            // app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
