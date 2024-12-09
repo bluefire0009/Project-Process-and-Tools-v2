335ab1a55f7e288d97e3867b80e468d92f8aa257
JohnSpruijt2 <johnspruijt@outlook.com>
2024-12-09 01:09:16 +0100

    Added softdelete functionality

--- CargoHub/Services/ItemTypesDBStorage.cs
+++ CargoHub/Services/ItemTypesDBStorage.cs
@@ -31,7 +31,8 @@ public class ItemTypesDBStorage : IItemTypeStorage
         ItemType? itemTypeInDb = await db.ItemTypes.FirstOrDefaultAsync(_ => _.Id == id);
         if (itemTypeInDb == null) return false;
 
-        db.ItemTypes.Remove(itemTypeInDb);
+        itemTypeInDb.IsDeleted = true;
+        db.ItemTypes.Update(itemTypeInDb);
         await db.SaveChangesAsync();
         return true;
     }
