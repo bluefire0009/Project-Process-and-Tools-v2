29b5c8e49105d7af69436a87ccfb3623e4e1656e
TurboGreaser <creamsupreme666@gmail.com>
2025-01-07 14:18:12 +0100

    addded [ExcludeFromCodeCoverage]

--- CargoHub/Migrations/20241217091855_b5.cs
+++ CargoHub/Migrations/20241217091855_b5.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace CargoHub.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class b5 : Migration
     {
         /// <inheritdoc />
