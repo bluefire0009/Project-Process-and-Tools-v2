4b48467cb0107ef97cb677786253a5b439745354
MilanVHR <1034676@hr.nl>
2025-01-17 15:38:33 +0100

    Client inetragrion test delete things that werent needed

--- IntegrationTests/client_test.cs
+++ IntegrationTests/client_test.cs
@@ -21,39 +21,5 @@ namespace IntegrationTests
                 new Client(){Id = 2, Name = "Bram", Address = "Jasmijnstraat 153", City = "Papendrecht", ZipCode = "9000 CG", Province = "Zuid-Holland", Country = "Nederland", ContactName = "Bram Nederlof", ContactPhone = "06123456789", ContactEmail = "Bram22nederlof@gmail.com"},
             ];
         private HttpClient client;
-
-        [TestInitialize]
-        public void Setup()
-        {
-            // Get the service provider and create a new scope for each test
-            var scope = Services.CreateScope();
-            _dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
-
-            // Reset the database to ensure it is clean for each test
-            _dbContext.Database.EnsureDeleted();  // Delete any existing database
-            _dbContext.Database.EnsureCreated();  // Create a new fresh database
-            client = CreateClient();
-            addTestClientsToDB(client);
-        }
     }
-     private void addTestClientsToDB(HttpClient client)
-        {
-            // Add both Clients to db
-            foreach(Client client in testClients)
-            {
-                string jsonData = JsonConvert.SerializeObject(client);
-                HttpContent postContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-                client.PostAsync($"{ClientsUrl}", postContent).GetAwaiter().GetResult();
-            }
-    
-    }
-    [TestCleanup]
-        public void CleanUp()
-        {
-            if (_dbContext != null)
-            {
-                _dbContext.Database.EnsureDeleted();  // Optionally delete the database
-            }
-        }
 }
-        
