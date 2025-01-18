357f7906cc006769b68e7c452c21f1f7363bd0b9
MilanVHR <1034676@hr.nl>
2025-01-18 21:53:54 +0100

    line 36 Change <= to <

--- CargoHub/Services/InventoriesDBStorage.cs
+++ CargoHub/Services/InventoriesDBStorage.cs
@@ -33,11 +33,12 @@ public class InventoriesDBStorage : IInventoryStorage
     public async Task<bool> addInventory(Inventory inventory)
     {
         if (inventory == null) return false;
-        if (inventory.Id <= 0) return false;
+        if (inventory.Id < 0) return false;
 
         Inventory? inventoryInDatabase = await db.Inventories.Where(w => w.Id == inventory.Id).FirstOrDefaultAsync();
         if (inventoryInDatabase != null) return false;
 
+        inventory.Id = 0;
         inventory.CreatedAt = CETDateTime.Now();
         inventory.UpdatedAt = CETDateTime.Now();
 
