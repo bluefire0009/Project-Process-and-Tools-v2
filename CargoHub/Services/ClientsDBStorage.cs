15e87f6203c2c20aa5486c57f11895f55647afea
MilanVHR <1034676@hr.nl>
2025-01-19 00:43:29 +0100

    Change every <= to <

--- CargoHub/Services/ClientsDBStorage.cs
+++ CargoHub/Services/ClientsDBStorage.cs
@@ -30,7 +30,7 @@ public class ClientDBStorage : IClientStorage
     public async Task<bool> addClient(Client client)
     {
         if (client == null) return false;
-        if (client.Id <= 0) return false;
+        if (client.Id < 0) return false;
 
         Client? clientInDatabase = await db.Clients.Where(c => c.Id == client.Id).FirstOrDefaultAsync();
         if (clientInDatabase != null) return false;
@@ -43,7 +43,7 @@ public class ClientDBStorage : IClientStorage
     }
     public async Task<bool> deleteClient(int id)
     {
-        if (id <= 0) return false;
+        if (id < 0) return false;
 
         Client? clientInDatabase = await db.Clients.Where(c => c.Id == id).FirstOrDefaultAsync();
         if (clientInDatabase == null) return false;
@@ -57,7 +57,7 @@ public class ClientDBStorage : IClientStorage
     public async Task<bool> updateClient(int idToUpdate, Client? updatedClient)
     {
         if (updatedClient == null) return false;
-        if (idToUpdate <= 0 || updatedClient.Id <= 0) return false;
+        if (idToUpdate < 0 || updatedClient.Id < 0) return false;
 
         Client? clientInDatabase = await db.Clients.Where(c => c.Id == idToUpdate).FirstOrDefaultAsync();
         if (clientInDatabase == null) return false;
@@ -75,7 +75,7 @@ public class ClientDBStorage : IClientStorage
 
     public IEnumerable<Order>? getClientOrders(int clientId)
     {
-        if (clientId <= 0) return null;
+        if (clientId < 0) return null;
 
         IEnumerable<Order> orders = db.Orders.Where(o => o.BillTo == clientId || o.ShipTo == clientId);
         return orders;
